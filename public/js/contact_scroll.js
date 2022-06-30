663f523c93a36333a1406e14d6eed17c6a7ed22a
Alamgir-K <k.alamgir2002@gmail.com>
2022-06-30 02:54:06 -0400

    added scrollreveal

---
+++ public/js/contact_scroll.js
@@ -0,0 +1,3 @@
+sr = ScrollReveal();
+sr.reveal('.contact-title', {duration:2000, delay: 200})
+sr.reveal('.contact-form', {duration:2000, scale:0.2})
---
+++ public/js/home_scroll.js
@@ -0,0 +1,15 @@
+sr = ScrollReveal();
+
+sr.reveal('.navbar-brand', {duration:2000, origin:'left', distance:'7rem'})
+sr.reveal('.nav-item', {duration:2000, origin:'right ', distance:'7rem'})
+sr.reveal('.analogue', {duration:2000, scale:0.2})
+sr.reveal('.title-first', {duration:2000, origin:'bottom', distance:'7rem'})
+sr.reveal('.title', {duration:2000})
+sr.reveal('.exp', {duration:2000, origin:'bottom', distance:'7rem', delay:400, useDelay: 'onload'})
+sr.reveal('.project-left', {duration:2000, origin:'left', distance:'7rem'})
+sr.reveal('.project-right', {duration:2000, origin:'right', distance:'7rem'})
+sr.reveal('.about-left', {duration:2000, origin:'left', distance:'7rem'})
+sr.reveal('.about-right', {duration:2000, origin:'right', distance:'7rem'})
+sr.reveal('.grid-1', {duration:2000, origin:'bottom', distance:'7rem'})
+sr.reveal('.grid-2', {duration:2000, origin:'bottom', distance:'7rem'})
+sr.reveal('.grid-3', {duration:2000, origin:'bottom', distance:'7rem'})
